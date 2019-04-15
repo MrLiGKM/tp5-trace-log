ae680253fe89aa5aa1cf01eee18f3c2af55cc123
LiGang <308529965@qq.com>
2019-04-15 15:48:05 +0800

    v1.0

--- src/config.php
+++ src/config.php
@@ -3,6 +3,13 @@
  * @Author: anchen
  * @Date:   2019-04-15 15:17:11
  * @Last Modified by:   anchen
- * @Last Modified time: 2019-04-15 15:18:49
+ * @Last Modified time: 2019-04-15 15:46:23
  */
-think\Config::set('test_config', 222);
+think\Route::group(
+    'traceloglist', [
+        ':id' => ['\mrlig\tracelog\controller\Tracelog@detail', ['method' => 'get'], ['id' => '\d+']],
+        'del' => ['\mrlig\tracelog\controller\Tracelog@delete', ['method' => 'post']],
+        'clear' => ['\mrlig\tracelog\controller\Tracelog@clear', ['method' => 'post']],
+        'list' => ['\mrlig\tracelog\controller\Tracelog@list', ['method' => 'get']],
+        '/' => ['\mrlig\tracelog\controller\Tracelog@index', ['method' => 'get']],    
+]);
